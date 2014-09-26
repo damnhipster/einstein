23eec1149656e59b998dd8e83c101c2ab9119062
damnhipster <hamant.brahmbhatt@gmail.com>
2014-09-26 22:11:20 +0100

    HB: Getting app to work on heroku

--- app.js
+++ app.js
@@ -2,12 +2,13 @@ var express = require('express');
 var app = express();
 
 app.set('views', __dirname + '/app');
+var port = process.env.PORT || 3000
 app.engine('html', require('ejs').renderFile);
 
 app.get('/', function(request, response) {
   response.render('index.html');
 });
 
-var server = app.listen(3000, function() {
-  console.log('Listening on port %d', server.address().port);
+var server = app.listen(port, function() {
+  console.log('Listening on port %d', port);
 });
