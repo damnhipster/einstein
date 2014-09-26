95c9f746be2ea4fe9fb3975f7a5904365db17521
damnhipster <hamant.brahmbhatt@gmail.com>
2014-09-26 18:58:12 +0100

    Rendering an index page

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 var express = require('express');
 var app = express();
 
+app.set('views', __dirname + '/app');
+app.engine('html', require('ejs').renderFile);
+
 app.get('/', function(request, response) {
-  response.send('Einstein');
+  response.render('index.html');
 });
 
 var server = app.listen(3000, function() {
