d110d94f8fda2ae149c76d7e256da1a321b67d7e
damnhipster <hamant.brahmbhatt@gmail.com>
2014-10-03 09:19:48 +0100

    Got test passing

--- build/app.js
+++ build/app.js
@@ -2,12 +2,20 @@
 var Einstein = React.createClass({displayName: 'Einstein',
   render: function () {
     return (
-      React.DOM.h1(null, "Einstein will find who you're looking for")
+      React.DOM.div({id: "einstein"}, 
+        React.DOM.h1(null, "Einstein will find who you're looking for"), 
+        React.DOM.h2(null, "Emberinos"), 
+        React.DOM.ul(null, 
+          React.DOM.li(null, "Aaron Chambers"), 
+          React.DOM.li(null, "Hem Brahmbhatt (", React.DOM.a({href: "http://github.com/damnhipster"}, "damnhipster"), ")"), 
+          React.DOM.li(null, "Chris Game")
+        )
+      )
     );
   }
 });
 
 React.renderComponent(
   Einstein(null),
-  document.getElementById('einstein')
+  document.getElementById('app')
 );
--- src/app.js
+++ src/app.js
@@ -2,12 +2,28 @@
 var Einstein = React.createClass({
   render: function () {
     return (
-      <h1>Einstein will find who you're looking for</h1>
+      <div id='einstein'>
+        <h1>Einstein will find who you're looking for</h1>
+        <Ember/>
+      </div>
+    );
+  }
+});
+
+var Ember = React.createClass({
+  render: function () {
+    return (
+      <h2>Emberinos</h2>
+      <ul>
+        <li>Aaron Chambers</li>
+        <li>Hem Brahmbhatt (<a href="http://github.com/damnhipster">damnhipster</a>)</li>
+        <li>Chris Game</li>
+      </ul>
     );
   }
 });
 
 React.renderComponent(
   <Einstein/>,
-  document.getElementById('einstein')
+  document.getElementById('app')
 );
